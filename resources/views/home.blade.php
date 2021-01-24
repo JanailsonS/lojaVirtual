bf2a30a4240f2b2df62514f1cfd898f554d835b1
JanailsonS <janailson.sousa@hotmail.com>
2021-01-24 01:34:32 -0300

    git init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>LojaVirtual</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+ </head>
+
+ <body>
+    <nav class="navbar navbar is-light pt-5 pr-6 pl-6  mr-0">
+        <a href="#" class="navbar-brand">LojaVirtual</a>
+        <div class="nav-menu ">
+            <div class="navbar-nav">
+                <a class="nav-item ml-5" >Home</a>
+                <a class="nav-item ml-5">Categorias</a>
+                <a class="nav-item ml-5">Cadastrar</a>
+             </div>
+        </div>
+        <div class="navbar-end">
+        <a href="#" class="btn btn-sm"><i class="fa fa-shopping-cart"></i></a>
+        </div>
+     </nav>
+
+     <div class="container">
+        <div class="columns">
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto1.png">
+                </figure>
+            </div>
+        
+        
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto2.png">
+                </figure>
+            </div>
+        
+        
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto3.jpg">
+                </figure>
+            </div>
+        
+
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto4.jpg">
+                </figure>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto5.jpg">
+                </figure>
+            </div>
+        
+
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto6.jpg">
+                </figure>
+            </div>
+
+                <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto7.jpg">
+                </figure>
+            </div>
+        
+
+            <div class="column">
+                <figure class="img is-200x200">
+                    <img src="http://localhost/imagens/produto8.jpg">
+                </figure>
+            </div>
+        
+         </div>
+
+
+      </div>
+
+ </body>
+
+
+</html>
